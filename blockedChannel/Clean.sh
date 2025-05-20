7f22adfc8d84df707eb98668c47d1e827a554d0c
chigovvv <kozhettop22@gmail.com>
2025-05-20 12:52:54 +0300

    html

---
+++ blockedChannel/Clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+rm -rf 1* 
+rm -rf 2* 
+rm -rf 3* 
+rm -rf 4* 
+rm -rf 5* 
+rm -rf 6* 
+rm -rf 7* 
+rm -rf 8* 
+rm -rf 9* 
+rm -rf log 
+rm -rf constant/polyMesh 
+
+echo clean
